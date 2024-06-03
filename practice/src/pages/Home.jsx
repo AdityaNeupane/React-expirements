2f1586f1beb98a420a1a708d23658c96db93b3f1
adityaneupane <bijayneupane324@gmail.com>
2024-06-03 18:27:30 +0545

    new changes

--- practice/src/pages/Home.jsx
+++ practice/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Task from '../components/Task';
+import { Task2 } from '../components/Task2';
+import { Task3 } from '../components/Task3';
 const Home = () => {
   return (
      <>
       <Task/>
+      <Task2/>
+      <Task3/>
     </>
   )
 }
